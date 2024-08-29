c270e03b0b192067286e0fd9c1a28b3b26ad9b08
Skip Miller <skip@thinktankconsulting.net>
2024-08-29 11:52:30 -0600

    Added Readme

--- tempCodeRunnerFile.sh
+++ tempCodeRunnerFile.sh
@@ -1,9 +1,1 @@
-
-# Initialize the directory as a Git repository
-git init
-
-# Add all files to the staging area
-git add .
-
-# Commit the files
-git commit -m "Initial commit"
+gh auth login --with-token
